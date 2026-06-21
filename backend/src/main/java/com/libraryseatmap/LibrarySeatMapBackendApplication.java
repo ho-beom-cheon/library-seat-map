88bb853ec3ed65abb5d1ff0a22dd914223362fa0
ho-beom-cheon <cjs414ghqja@gmail.com>
2026-06-21 22:04:42 +0900

    feat: set up project foundation

---
+++ backend/src/main/java/com/libraryseatmap/LibrarySeatMapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.libraryseatmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySeatMapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySeatMapBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/libraryseatmap/LibrarySeatMapBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryseatmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySeatMapBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
